d317c7c6661743f3f0a029a428ca0eaf5c50fa5c
Elsie Koglin <elsieweinzierl@gmail.com>
2016-12-22 13:20:58 -0600

    Completed 6.2 pairing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -11,96 +11,101 @@
 # Our initialize method didn't do anyything when it was misspelled.
 
 
+
+#Driver Code:
+#Puppy.new
+#spot = Puppy.new
+#fido = Puppy.new
+#duchess = Puppy.new
+
+
+# Step 1 in Release 0 did NOT work for us. We couldn't figure out the prompt.
+# Our initialize method didn't do anything when it was misspelled.
+
+
 class Puppy
 
-  def initialize
-    #puts "Initializing new puppy instance #..."
-  end
-  #puts initialize
+def initialize
+  p "Initializing new puppy instance ..."
+end
+#puts initialize
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-  end
-  #Test: p fetch(ball)
+def fetch(toy)
+  puts "I brought back the #{toy}!"
+end
+#Test: p fetch(ball)
 
-  def speak (x)
-  	puts "Woof!" * (x)
-  end
-  #Test: p speak(5)
+def speak (x)
+    p "Woof!" * (x)
+end
+#Test: p speak(5)
 
-  def roll_over
-  	puts "*rolls over*"
-  end
-  #Test: p roll_over
+def roll_over
+    p "*rolls over*"
+end
+#Test: p roll_over
 
-  def dog_years(y)
-  	puts (y * 7)
-  end
-  #Test: p dog_years(12)
+def dog_years(y)
+    p (y * 7)
+end
+#Test: p dog_years(12)
 
-  def play_dead
-  	puts "*playing dead*"
-  end
-  # #Test: p play_dead
+def play_dead
+    p "*playing dead*"
+end
+# #Test: p play_dead
 
 end
+Spot = Puppy.new
+Fido = Puppy.new
+Duchess = Puppy.new
 
-p Puppy.new.fetch("ball")
-p Puppy.new.speak(5)
-p Puppy.new.roll_over
-p Puppy.new.dog_years(12)
-p Puppy.new.play_dead
+p Spot.fetch("ball")
+p Spot.speak(5)
+p Spot.roll_over
+p Spot.dog_years(12)
+p Spot.play_dead
 
 
-
-#Cat.new
-#Ralph = Cat.new(@name)
-#Fluffy = Cat.new
-#Caeli = Cat.new
+###########
 
 class Cat
-    def itiliaze
+    def initialize(coat_color)
+        @coat_color = coat_color
     end
 
     def speak (x)
-    puts "Meow!" * (x)
+    p "Meow!" * (x)
     end
 
     def scratch
-        puts "*scratching*"
+        p "*scratching*"
     end
 
     def nap
-        puts "*napping*"
+        p "*napping*"
     end
+
 end
+Ralph = Cat.new("Brown")
+Fluffy = Cat.new("Stripped")
+Caeli = Cat.new("white")
 
-ralph = Cat.new
-fluffy = Cat.new
-caeli = Cat.new
+p Ralph.speak(3)
 
-puts ralph.speak(3)
-
-
-#Release 2
     counter = 50
+    pride = []
     until counter == 0
-        #puts Cat.new.Caeli
-        puts Ralph
-        #puts Cat.new.Fluffy
+        new_cat = Cat.new("Brown")
+        pride << new_cat
         counter -= 1
     end
 
-# putting loop into data structure
-#initializing cat.new into the cat names 
-#so that we can apply all of the methods to each cat
-data_structure.each do |ralph, fluffy, caeli|
-    puts |ralph|.speak.scratch.nap
-    puts |fluffy|.speak.scratch.nap
-    puts |caeli|.speak.scratch.nap
+pride.each do |feline|
+    feline.speak(3)
+    feline.scratch
+    feline.nap    
 end
-
-
 
 
 
