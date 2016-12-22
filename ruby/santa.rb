e40eda1aae61e068d69302d531780a6a61775b92
Elsie Koglin <elsieweinzierl@gmail.com>
2016-12-21 20:06:52 -0600

    Completed 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,42 +2,43 @@
 #methods (initialize, speak, milk & cookies).
 
   class Santa
-    attr_reader :age, :ethnicity, :hat_size, :cookie_number
-    attr_accessor :gender
+    attr_reader :age, :ethnicity, :cookie_number
+    attr_accessor :gender, :ethnicity
   
-  	def initialize(size)
-  		@gender = "female"
-  		@ethnicity = "white"
-  		@hat_size = size
+  	def initialize
+  		@gender = gender
+  		@ethnicity = ethnicity
   		puts "Initializing Santa instance ..."
-  		puts "My hat size is: #{@hat_size}."
+  		
   	end
+
+  	def age
+  	   age = (1..140).to_a.sample
+  	end
+
+  	#def speak
+  	#  puts "Ho, ho, ho! Haaaappy holidays!"	
+  	#end
+  	
+  	#def celebrate_birthday(x)
+  	#  x + 1
+  	#end
+  	
+  	#def get_mad_at(reindeer_name) 
+  	#  @reindeer_ranking = ["Rudolph",   "Dasher", "Dancer", "Prancer",   "Vixen", "Comet", "Cupid", "Donner",   "Blitzen"]
+  	#  @reindeer_ranking.delete(reindeer_name)
+  	#  @reindeer_ranking.push(reindeer_name)
+  	# end
   
-  	def speak
-  	  @reindeer_ranking = ["Rudolph",   "Dasher", "Dancer", "Prancer",   "Vixen", "Comet", "Cupid", "Donner",   "Blitzen"]
-  	  
-  	  @age = 0
-  	  puts "Ho, ho, ho! Haaaappy holidays!"	
-  	end
-  	
-  	def celebrate_birthday(x)
-  	  x + 1
-  	end
-  	
-  	def get_mad_at(reindeer_name) 
-  	  @reindeer_ranking.delete(reindeer_name)
-  	  @reindeer_ranking.push(reindeer_name)
-  	 end
-  
-  	def eat_milk_and_cookies(cookie)
-  		@cookie_number = 4
-  		puts "That was a good #{cookie}   cookie! I actually ate #{@cookie_number}."
-  	end
-  	
-  	def travel(city)
-  	  @city = city
-  	 puts "The first stop on my trip will be #{@city}."
-  	end
+  	#def eat_milk_and_cookies(cookie)
+  	#	@cookie_number = 4
+  	#	puts "That was a good #{cookie}   cookie! I actually ate #{@cookie_number}."
+  	#end
+  	#
+  	#def travel(city)
+  	#  @city = city
+  	# puts "The first stop on my trip will be #{@city}."
+  	#end
   
   	#getter methods for attributes before refactoring
   		#def age
@@ -48,9 +49,6 @@
   		#  @ethnicity
   		#end
   		#
-  		#def hat_size
-  		#  @hat_size
-  		#end
   #	
   		#def cookie_number
   		#  @cookie_number
@@ -64,26 +62,21 @@
   
   
   #Driver Code
-  santa = Santa.new(30)
-  santa.speak
-  santa.eat_milk_and_cookies("gingrbread")
-  santa.travel("Chicago")
-  puts santa.gender = "Thats personal."
-  puts santa.get_mad_at("Cupid")
-  puts santa.celebrate_birthday(25)
-  
-  #Testing: getter methods printed out
-  #Age was set to 0
-  puts "--------#{santa.age}, #{santa.ethnicity}, #{santa.hat_size}, #{santa.cookie_number}--------"
+  santa = Santa.new
+  puts santa.gender = ["male", "female", "gender fluid"].sample
+  puts santa.age
+  puts santa.ethnicity = ["black", "Latino", "white", "unicorn"].sample
+  #santa.speak
+  #santa.eat_milk_and_cookies("gingrbread")
+  #santa.travel("Chicago")
+  #santa.cookie_number
+  #santa.age
 
-
-
-
-
-
-
-
-
+ 
+23.times{
+puts santa.gender = ["male", "female", "gender fluid"].sample
+puts santa.age
+puts santa.ethnicity = ["black", "Latino", "white", "unicorn"].sample}
 
 
 
