e3c819e2ae9e33ea23d3c8dcdd919fda0524fb63
Elsie Koglin <elsieweinzierl@gmail.com>
2016-12-21 18:51:58 -0600

    Progress through refactor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,52 +1,84 @@
-#Create a Santa class continaing three methods (initialize, speak, milk & cookies).
+#Create a Santa class continaing three 
+#methods (initialize, speak, milk & cookies).
+
+  class Santa
+    attr_reader :age, :ethnicity, :hat_size, :cookie_number
+    attr_accessor :gender
+  
+  	def initialize(size)
+  		@gender = "female"
+  		@ethnicity = "white"
+  		@hat_size = size
+  		puts "Initializing Santa instance ..."
+  		puts "My hat size is: #{@hat_size}."
+  	end
+  
+  	def speak
+  	  @reindeer_ranking = ["Rudolph",   "Dasher", "Dancer", "Prancer",   "Vixen", "Comet", "Cupid", "Donner",   "Blitzen"]
+  	  
+  	  @age = 0
+  	  puts "Ho, ho, ho! Haaaappy holidays!"	
+  	end
+  	
+  	def celebrate_birthday(x)
+  	  x + 1
+  	end
+  	
+  	def get_mad_at(reindeer_name) 
+  	  @reindeer_ranking.delete(reindeer_name)
+  	  @reindeer_ranking.push(reindeer_name)
+  	 end
+  
+  	def eat_milk_and_cookies(cookie)
+  		@cookie_number = 4
+  		puts "That was a good #{cookie}   cookie! I actually ate #{@cookie_number}."
+  	end
+  	
+  	def travel(city)
+  	  @city = city
+  	 puts "The first stop on my trip will be #{@city}."
+  	end
+  
+  	#getter methods for attributes before refactoring
+  		#def age
+  		#  @age
+  		#end
+  #	
+  		#def ethnicity
+  		#  @ethnicity
+  		#end
+  		#
+  		#def hat_size
+  		#  @hat_size
+  		#end
+  #	
+  		#def cookie_number
+  		#  @cookie_number
+  		#end
+  
+  	#setter methods before refactoring
+  		#def gender=(new_gender)
+  		#  @gender = new_gender
+  		#end
+  end
+  
+  
+  #Driver Code
+  santa = Santa.new(30)
+  santa.speak
+  santa.eat_milk_and_cookies("gingrbread")
+  santa.travel("Chicago")
+  puts santa.gender = "Thats personal."
+  puts santa.get_mad_at("Cupid")
+  puts santa.celebrate_birthday(25)
+  
+  #Testing: getter methods printed out
+  #Age was set to 0
+  puts "--------#{santa.age}, #{santa.ethnicity}, #{santa.hat_size}, #{santa.cookie_number}--------"
 
 
-class Santa
-
-	def initialize(size)
-		puts "Initializing Santa instance ..."
-		@gender = "female"
-		@ethnicity = "white"
-		@hat_size = size
-		puts "My hat size is: #{size}."
-	end
-
-	def speak
-	  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-	  @age = 0
-	  puts "Ho, ho, ho! Haaaappy holidays!"	
-	end
-
-	def eat_milk_and_cookies(cookie)
-		@cookie_number = 4
-		puts "That was a good #{cookie} cookie! I actually ate #{@cookie_number}."
-	end
-
-	#getter methods for attributes
-	def hat_size
-	  @hat_size
-	end
-
-	def cookies_eaten
-	  @cookie_number
-	end
-
-	#setter methods
-	#def first_stop=(destination)
-	#	@first_stop = destination
-	#end
-
-end
 
 
-#Driver Code
-elf = Santa.new(30)
-elf.speak 
-elf.eat_milk_and_cookies("gingrbread")
-
-
-#getter methods printed out
-puts "#{elf.hat_size}, #{elf.cookies_eaten}"
 
 
 
