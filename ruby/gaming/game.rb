6b0341846e85af6e1b4dc4a05ad2bf5224442039
Elsie Koglin <elsieweinzierl@gmail.com>
2016-12-23 16:39:57 -0600

    Completed solo challenege week 6

--- ruby/gaming/game.rb
+++ ruby/gaming/game.rb
@@ -1,47 +1,15 @@
 
-##One player will provide the secret word, the second player will guess the entry ╨ one letter at a time.
-#-input: secret word(all lowercase, no special characters)
-#-method: split the secret word into individual letters. Add the letters to an empty array
-#-output: hidden word (something like ╥- - - - - - -╥
-#
-#
-#
-#
-##Repeated guesses do not count against the player.
-#-input: guessed letter
-#-method: add guessed letter to second/separate array.
-#  -it letter matches within the guesses letter array, do not count as turn.
-#-output: error message saying letter has already been guessed.
-#
-#
-## Feedback delivered to the player. If the guessed letter matches within the secret word, reveal letter to user.
-#-input: single letter(all instances if applicable)
-#-method: if letter matches secret word array, print letter in place. If letter is incorrect (and not previously guessed) subtract from number of turns.
-#-output: revealed letter if correct. Warning if incorrect.
-#
-##End of guesses, congratulate player or taunt.
-#-input: all guesses
-#-method: when guesses have been exhausted, congratulate. If exhausted and incorrect, tunt. 
-#-output: Congratulatory or taunting method.
-
-
-
-
-##Number of guesses will be limited to the length of the secret word plus 2 (they will receive two additional guesses beyond the length of the secret word).
-#-input: secret word
-#-method: Add 2 to length of secret word array. 
-#-output: number of guesses remaining
-
 class Game
-  attr_reader :guess_count, :first_letter
-  attr_accessor :letters_guessed, :word
+  attr_reader :guess_count, :first_letter, :word
+  attr_accessor :letters_guessed, :word, :guess_count
 
+##One player will provide the secret word, the second player will guess the entry as a whole word.
+#-input: secret word (assumed: all lowercase, no special characters)
+#-method: split the secret word into individual letters. Add the letters to an empty array.
+#-output: an array continaing the individual letters. 
   def initialize(word)
-    p word.split("")
+    word.split("")
     @word = word
-    @first_letter = word.byteslice(0) 
-    p @guess_count = word.length + 2
-    @letters_guessed = []
   end
 
   def length
@@ -51,48 +19,82 @@ class Game
   def first_letter
     @first_letter = @word.byteslice(0)
   end
-  
- 
+
+  def last_letter
+    @last_letter = @word.byteslice(-1)
+  end
+
+##Number of guesses will be limited to the length of the secret word plus 2 
+#-input: secret word
+#-method: Add 2 to length of secret word. 
+#-output: number of guesses remaining
+  def guess_count
+    @guess_count = @word.length + 2
+  end 
 end
 
+####### UPDATE SECRET WORD########
+#Driver Code
 game = Game.new("unicorn")
-p game.length
+game.length
+game.guess_count
+
+puts "The secret word is #{game.length} letters long.
+It begins with the letter '#{game.first_letter.upcase}'
+and ends with the letter '#{game.last_letter.upcase}'.
+You have #{game.guess_count} guesses to start the game."
 
 
-puts "The secret word is #{game.length} letters long. It begins with the letter #{game.first_letter}."
-  
-# def check_cup(index)
-#  	@guess_count += 1
-#  	if @cups[index] == "ball"
-#  		@is_over = true
-#  	else
-#  		false
-#  	end
-#  end
+## Feedback delivered to the player. If the guessed word matches the secret word end game.
+#-input: guessed word
+#-method: if word matches secret word, exit and print congratulations. 
+#-method: if word does not match prompt to try again. 
+#-output: congratulations or new entry opportunity.
+
+counter = game.guess_count
+  until counter == 0
+    puts "Please enter a guess"
+    guess = gets.chomp
+    if guess == game.word
+      puts "Congrats, you won!"
+      break
+    else puts "Nope, try again!"
+    end
+    counter -= 1
+  end
+
+puts "Thanks for playing. It's been fun."
+
+
+
+
+## ALTERNATIVE CODE THAT DIDN'T WORK/I DIDN'T USE:
+## I counldn't get the letters to loop the way I wanted.
+## I wasnt able to discount previous guesses for number of turns.
+
+
+#def letters_guessed(letter)
+#    @letters_guessed = []
+#    @letters_guessed << letter
 #end
 
-# user interface
 
-#puts "Welcome to the Cup Game!"
-#game = CupGame.new
+#puts "Please enter a letter to guess:"
+#@letter = gets.chomp
 #
-#puts "Shuffling cups ..."
-#game.shuffle
 #
-#while !game.is_over
-#  puts "Which cup has the ball? Enter a guess of 1, 2, or 3:"
-#  guess = gets.chomp.to_i
-#  if !game.check_cup(guess - 1)
-#  	puts "Nope! Try again."
-#  end
+#counter = game.guess_count
+#  until counter == 0
+#case 
+# when "unicorn".include?(@letter) == true
+#   puts "Next guess?"
+#   letter = gets.chomp
+# else
+#   puts "Nope! Not included. You now have #{game.guess_count -= 1} guesses remaining. Please guess again."
+#   letter = gets.chomp
+# end
+# game.guess_count -= 1
 #end
-#
-#puts "You won in #{game.guess_count} guesses!"
-
-
-
-
-
 
 
 
