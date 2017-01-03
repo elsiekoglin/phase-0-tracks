55ab982139c645130da8e637a78e4691ec75533e
Elsie Koglin <elsieweinzierl@gmail.com>
2017-01-03 17:55:25 -0600

    progress through step 3

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,12 @@
- 
+ // Create a function that takes a string as the parameter and reverses the string.
+
+
+
+function reverse(string) {
+  return string.split("").reverse().join("");
+}
+
+console.log(reverse("hello"));
+
+
+
