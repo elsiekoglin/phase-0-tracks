9fa2cbad64f30bb1d8027ab0aac80d0ac9a65f77
Elsie Koglin <elsieweinzierl@gmail.com>
2017-01-14 15:20:31 -0600

    Initial commit. Created files, bd and required sqlite3.

---
+++ databases/pizza/pizza_maker.rb
@@ -0,0 +1,37 @@
+require 'sqlite3'
+
+#db = SQLite3::Database.new("pizzas.db")
+#
+#create_table_cmd = <<-SQL
+#  CREATE TABLE IF NOT EXISTS pizzas(
+#    id INTEGER PRIMARY KEY,
+#    base VARCHAR(255),
+#    toppings VARCHAR(255), 
+#    cheese VARCHAR(255),
+#    finishes VARCHAR(255),
+#  )
+#SQL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
