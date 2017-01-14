b4d8b0507428af109955f5159b3ff495e01e95e4
Elsie Koglin <elsieweinzierl@gmail.com>
2017-01-14 16:44:19 -0600

    Completed 8.5

--- databases/pizza/pizza_maker.rb
+++ databases/pizza/pizza_maker.rb
@@ -14,27 +14,35 @@ SQL
 
 db.execute(create_table_cmd)
 
-db.execute("INSERT INTO pizzas (base, toppings, cheese, finishes) VALUES ('garlic pesto', 'olives, pesto, artichokes, pepperoni', 'ricotta and mozzarella', 'salt/pepper and oregano')")
+
+####### All Test information:
+#------------------------------------------------------------------#
+## db.execute("INSERT INTO pizzas (base, toppings, cheese, finishes) VALUES ('garlic pesto', 'olives, pesto, artichokes, pepperoni', 'ricotta and mozzarella', 'salt/pepper and oregano')")
+
+## def make_a_pizza(base, toppings, cheese, finishes)
+##    "I will have a #{base} pizza, with #{toppings}, #{cheese}, and #{finishes}."
+## end
+
+## base = ["white", "garlic pesto", "classic red", "bbq sauce"].sample
+## toppings = ["artichokes", "olives", "spinach", "tomatoes", "onions", "peppers", "pepperoni", "sausage", "chicken"].sample
+## cheese = ["ricotta", "mozzarella", "parmesan"].sample
+## finishes = ["salt/pepper", "oregano", "fresh basil", "olive oil drizzle", "pesto", "bbq sauce"].sample
 
 
-def make_a_pizza(db, name, age)
-  db.execute("INSERT INTO pizza (base, toppings, cheese, finishes) VALUES (?, ?, ?, ?)", [base, toppings, cheese, finishes])
+puts "In Homer Simpson's voice: MMMMM..... PIZZA......"
+puts "How many pizzas would you like?"
+order = gets.chomp
+
+def make_a_pizza(db, base, toppings, cheese, finishes)
+  db.execute("INSERT INTO pizzas (base, toppings, cheese, finishes) VALUES (?, ?, ?, ?)", [base, toppings, cheese, finishes])
+  "Your pizza will have a #{base} base, with #{toppings}, #{cheese}, and #{finishes}."
 end
 
+order.to_i.times do
+p make_a_pizza(db, ["white", "garlic pesto", "classic red", "bbq"].sample, ["artichokes", "olives", "spinach", "tomatoes", "onions", "peppers", "pepperoni", "sausage", "chicken"].sample, ["ricotta", "mozzarella", "parmesan"].sample, ["salt/pepper", "oregano", "fresh basil", "olive oil drizzle", "pesto", "bbq sauce"].sample)
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+puts "Don't forget, every pizza is a personal pizza if you try hard and believe in yourself." 
 
 
 
