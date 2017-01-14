353ccf3c54719f6124fe8e12af1e370f28bcefbb
Elsie Koglin <elsieweinzierl@gmail.com>
2017-01-14 15:35:30 -0600

    Tested added single 'pizza' to database.

--- databases/pizza/pizza_maker.rb
+++ databases/pizza/pizza_maker.rb
@@ -1,20 +1,25 @@
 require 'sqlite3'
 
-#db = SQLite3::Database.new("pizzas.db")
-#
-#create_table_cmd = <<-SQL
-#  CREATE TABLE IF NOT EXISTS pizzas(
-#    id INTEGER PRIMARY KEY,
-#    base VARCHAR(255),
-#    toppings VARCHAR(255), 
-#    cheese VARCHAR(255),
-#    finishes VARCHAR(255),
-#  )
-#SQL
+db = SQLite3::Database.new("pizzas.db")
+
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS pizzas(
+    id INTEGER PRIMARY KEY,
+    base VARCHAR(255),
+    toppings VARCHAR(255), 
+    cheese VARCHAR(255),
+    finishes VARCHAR(255)
+  );
+SQL
+
+db.execute(create_table_cmd)
+
+db.execute("INSERT INTO pizzas (base, toppings, cheese, finishes) VALUES ('garlic pesto', 'olives, pesto, artichokes, pepperoni', 'ricotta and mozzarella', 'salt/pepper and oregano')")
 
 
-
-
+def make_a_pizza(db, name, age)
+  db.execute("INSERT INTO pizza (base, toppings, cheese, finishes) VALUES (?, ?, ?, ?)", [base, toppings, cheese, finishes])
+end
 
 
 
